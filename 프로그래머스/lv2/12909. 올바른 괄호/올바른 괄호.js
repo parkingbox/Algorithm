7f1c490d9238132bfce93a9b3d5f43809e61d315
Seungwoo Park <101634719+parkingbox@users.noreply.github.com>
2023-05-25 17:58:02 +0900

    [level 2] Title: 올바른 괄호, Time: 5.07 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,8 @@
+function solution(s){
+    let answer = 0
+    for(let i of s) {
+      i == "(" ? answer++ : answer--;
+        if(answer < 0) return false
+    }
+  return answer === 0 ? true : false;
+}
