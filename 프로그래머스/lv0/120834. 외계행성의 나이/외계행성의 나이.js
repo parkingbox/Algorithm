bb641c8b2537300f4279daf799283493446bbdb4
Seungwoo Park <101634719+parkingbox@users.noreply.github.com>
2023-06-01 21:44:30 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,15 @@
+function solution(age) {
+    const ageArr = String(age).split("");
+    
+    const ageIn962 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+    
+    let ans = "";
+    
+    for(let i = 0; i < ageArr.length; i++){
+        let index = Number(ageArr[i]);
+        
+        ans += ageIn962[index];
+    }
+    
+    return ans;
+}
