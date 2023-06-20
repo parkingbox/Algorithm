6d1f6f8c438ba1ae53296c5468e1fea0ec066260
Seungwoo Park <101634719+parkingbox@users.noreply.github.com>
2023-06-20 22:21:45 +0900

    [level 2] Title: 짝지어 제거하기, Time: 29.25 ms, Memory: 47 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,7 @@
+function solution(s) {
+    const stack = [];
+    for(let i = 0 ; i < s.length ; i++ ){
+      !stack.length || stack[stack.length-1] !== s[i] ? stack.push(s[i]) : stack.pop();
+    }
+    return stack.length ? 0 : 1;
+}
