81fcbe5fd5d99929baa7aa62090e2ed5d6059378
Seungwoo Park <101634719+parkingbox@users.noreply.github.com>
2023-06-25 19:26:21 +0900

    [unrated] Title: 2의 영역, Time: 0.30 ms, Memory: 41.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.js
@@ -0,0 +1,7 @@
+function solution(arr) {
+    const startIdx = arr.indexOf(2)
+    const endIdx = arr.lastIndexOf(2)
+    
+    return startIdx+endIdx< 1 ? [-1] : arr.slice(startIdx, endIdx+1)
+    
+}
